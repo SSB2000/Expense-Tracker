f532b2311b4ac59e822be818365cfebf0f3a345b
SSB2000 <mshubhambohra@gmail.com>
2025-08-03 21:10:27 +0530

    added protect moddule

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,5 +2,14 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.protects = async ( req, res, next ) => {
-    
-}
+    let token = req.headers.authorization?.split(' ')[1];
+    if ( !token ) return res.status(401).json({ message: 'Not authorized, no token' });
+
+    try {
+        const decode = jwt.verify( token, process.env.JWT_SECRET );
+        req.user = await User.findById( decoded.id ).select( '-password' );
+        next();
+    } catch ( err ) {
+        res.status( 401 ).json({ message: 'Not authorized, token failed' });
+    }
+};
