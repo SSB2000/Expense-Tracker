df8a7c5d26fa891562f813541ed91dded836fbd0
SSB2000 <mshubhambohra@gmail.com>
2025-08-03 16:08:18 +0530

    added login page

--- frontend/expense-tracker/src/pages/Auth/signUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/signUp.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout';
+import Input from '../../components/inputs/input';
+import { Link, useNavigate } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 export const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
   return (
     <div>signUp</div>
   )
