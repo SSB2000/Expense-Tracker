7535c6d7010e91c8e3f66a07e72112a7be03deb0
SSB2000 <mshubhambohra@gmail.com>
2025-08-04 11:13:37 +0530

    Integrated frontend with login APIs

--- frontend/expense-tracker/src/pages/Auth/login.jsx
+++ frontend/expense-tracker/src/pages/Auth/login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from '../../components/layouts/AuthLayout';
 import Input from '../../components/inputs/input';
 import { Link, useNavigate } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
@@ -29,7 +31,24 @@ export const Login = () => {
     setError("");
 
     // Login API Call;
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password.
+      });
+      const { token, user } = response.data;
 
+      if ( token ) {
+        localStorage.setItem( 'token', token );
+        navigate( '/dashboard' );
+      }
+    } catch ( error ) {
+      if ( error.response && error.response.data.message ) {
+        setError( error.response.data.message );
+      } else {
+        setError( 'Something went wrong. Please try again.' );
+      }
+    }
   }
 
   return (
