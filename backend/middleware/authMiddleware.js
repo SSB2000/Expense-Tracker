532dd4ed36f4e21d08ea70cd0ee41ae9c6240437
SSB2000 <mshubhambohra@gmail.com>
2025-08-04 08:38:21 +0530

    Added image upload middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.protects = async ( req, res, next ) => {
+exports.protect = async ( req, res, next ) => {
     let token = req.headers.authorization?.split(' ')[1];
     if ( !token ) return res.status(401).json({ message: 'Not authorized, no token' });
-
+    console.log(token);
     try {
         const decode = jwt.verify( token, process.env.JWT_SECRET );
-        req.user = await User.findById( decoded.id ).select( '-password' );
+        req.user = await User.findById( decode.id ).select( '-password' );
         next();
     } catch ( err ) {
         res.status( 401 ).json({ message: 'Not authorized, token failed' });
