1abc841f6bcd55e53eb7bba5abca0bdced4cc556
SSB2000 <mshubhambohra@gmail.com>
2025-08-07 04:35:42 +0530

    Added Dashboard Data 'Balance, Income and expense'

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { addThousandsSeparator } from '../../utils/helper';
+import InfoCard  from '../../components/Cards/InfoCard';
+
+import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
+import { IoMdCard } from 'react-icons/io';
 
 const Home = () => {
   useUserAuth();
@@ -10,12 +17,62 @@ const Home = () => {
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchDashboardData = async () => 
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log('Something went wrong. Please try again.', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  
+    return () => {
+      
+    }
+  }, [])
+  
 
   return (
     <DashboardLayout activeMenu='Dashboard'>
       <div className='my-5 mx=auto'>
-        Hello
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <InfoCard
+            icon={<IoMdCard />}
+            label='Total Balance'
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color='bg-primary'
+          />
+          <InfoCard
+            icon={<IoMdCard />}
+            label='Total Income'
+            value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+            color='bg-orange-500'
+          />
+          <InfoCard
+            icon={<IoMdCard />}
+            label='Total Expense'
+            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            color='bg-red-500'
+          />
+
+          <div className=''>
+
+          </div>
+        </div>
       </div>
     </DashboardLayout>
   );
