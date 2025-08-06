dd26f2e36608c0830da44365ae702b38620687a8
SSB2000 <mshubhambohra@gmail.com>
2025-08-07 04:00:40 +0530

    Added User Context to Dashboard Home

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  useUserAuth();
+
+  const navigate = useNavigate();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => 
+
   return (
     <DashboardLayout activeMenu='Dashboard'>
       <div className='my-5 mx=auto'>
