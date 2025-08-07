90e86f1b6f6d777b6c6ceadf962e4fd43edebd2d
SSB2000 <mshubhambohra@gmail.com>
2025-08-07 05:35:06 +0530

    Added recent transactions UI feature in dashboard home page

---
+++ frontend/expense-tracker/src/components/Dashboard/RecentTransactions.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+//import { IoMdDocument } from 'react-icons/io';
+import { LuArrowRight } from 'react-icons/lu';
+import moment from 'moment';
+import TransactionInfoCard from '../Cards/TransactionInfoCard';
+
+const RecentTransactions = ({ transactions, onSeeMore }) => {
+  return (
+    <div className='cards'>
+        <div className='flex items-center justify-between'>
+            <h5 className=''>
+                Recent Transactions
+            </h5>
+            <button
+                className='card-btn' onClick={onSeeMore}
+            >
+                See All <LuArrowRight className='text-base' />
+            </button>
+        </div>
+        
+        <div className='mt-6'>
+            {transactions?.slice(0,5)?.map((item) => {
+                return (
+                    <TransactionInfoCard
+                        key={item._id}
+                        title={item.type == 'expense' ? item.category : item.source}
+                        icon={item.icon}
+                        date={moment(item.date).format('Da MMM YYYY')}
+                        amount={item.amount}
+                        type={item.type}
+                        hideDeleteBtn
+                    />
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default RecentTransactions;
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -6,6 +6,7 @@ import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { addThousandsSeparator } from '../../utils/helper';
 import InfoCard  from '../../components/Cards/InfoCard';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import { IoMdCard } from 'react-icons/io';
@@ -57,13 +58,13 @@ const Home = () => {
             color='bg-primary'
           />
           <InfoCard
-            icon={<IoMdCard />}
+            icon={<LuWalletMinimal />}
             label='Total Income'
             value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
             color='bg-orange-500'
           />
           <InfoCard
-            icon={<IoMdCard />}
+            icon={<LuHandCoins />}
             label='Total Expense'
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color='bg-red-500'
@@ -72,6 +73,12 @@ const Home = () => {
           <div className=''>
 
           </div>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate('/expense')}
+          />
         </div>
       </div>
     </DashboardLayout>
